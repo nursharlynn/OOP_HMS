b9b9cf0541bf1a9c3194f5c3b1d61aa09a89a49a
nursharlynn <80143012+nursharlynn@users.noreply.github.com>
2024-11-07 17:35:08 +0800

    Add new files via upload
    Re-uploaded updated HMS directory with corrected errors and missing imports.

---
+++ HMS/Appointment/CompletedAppointment.java
@@ -0,0 +1,21 @@
+package Appointment;
+
+public class CompletedAppointment extends Appointment {
+
+	private AppointmentOutcome outcome;
+
+	/**
+	 * 
+	 * @param appointment
+	 * @param outcome
+	 */
+	public CompletedAppointment(Appointment appointment, AppointmentOutcome outcome) {
+		// TODO - implement CompletedAppointment.CompletedAppointment
+		throw new UnsupportedOperationException();
+	}
+
+	public AppointmentOutcome getOutcome() {
+		return this.outcome;
+	}
+
+}
---
+++ HMS/src/Doctor/IAppointment.java
@@ -0,0 +1,26 @@
+package Doctor;
+
+import Appointment.*;
+
+public interface IAppointment {
+
+	/**
+	 * 
+	 * @param doctor
+	 */
+	boolean reviewAppointmentRequests(Doctor doctor);
+
+	/**
+	 * 
+	 * @param doctor
+	 * @param appointmentId
+	 */
+	void recordAppointmentOutcome(Doctor doctor, int appointmentId);
+
+	/**
+	 * 
+	 * @param patientId
+	 */
+	ArrayList<CompletedAppointment> viewPastAppointmentOutcomeRecord(String patientId);
+
+}
